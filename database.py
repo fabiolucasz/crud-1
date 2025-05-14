c7064b7bd732c2afd21b13bbc8ed4eccb27a25ef
FABIO L SANTOS <78166985+fabiolucasz@users.noreply.github.com>
2025-05-14 18:34:13 -0300

    Update database.py

--- database.py
+++ database.py
@@ -20,25 +20,58 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 #adicionar usario
-novo_aluno = Aluno(nome="Alice", idade=50)
-session.add(novo_aluno)
-session.commit()
-
-# Buscar aluno
-aluno = session.query(Aluno).filter(Aluno.nome == "Alice").first()
-
-if aluno:
-    aluno.idade = 18
+def adicionar_usuario():
+    novo_aluno = Aluno(nome="Alice", idade=50)
+    session.add(novo_aluno)
     session.commit()
-    print("Aluno atualizado com sucesso!")
-else:
-    print("Aluno não encontrado.")
 
 
+def alterar_dados():
+    aluno = session.query(Aluno).filter(Aluno.nome == "Alice").first()
 
+    if aluno:
+        aluno.idade = 18
+        session.commit()
+        print("Aluno atualizado com sucesso!")
+    else:
+        print("Aluno não encontrado.")
 
+# Buscar aluno para excluir
+def excluir_aluno():
+    aluno = session.query(Aluno).filter(Aluno.id == 2).first()
 
+    if aluno:
+        session.delete(aluno)
+        session.commit()
+        print("Aluno removido com sucesso!")
+    else:
+        print("Aluno não encontrado.")
 
-alunos = session.query(Aluno).all()
-for aluno in alunos:
-    print(f"ID: {aluno.id} | Nome: {aluno.nome} | Idade: {aluno.idade}")
+def listar_alunos():
+    alunos = session.query(Aluno).all()
+    for aluno in alunos:
+        print(f"ID: {aluno.id} | Nome: {aluno.nome} | Idade: {aluno.idade}")
+
+#Criando a janela
+import tkinter as tk
+
+janela = tk.Tk()
+janela.geometry("400x500")
+
+#label nome e entry
+label_nome = tk.Label(janela,text="Nome")
+label_nome.pack()
+entry_nome = tk.Entry(janela)
+entry_nome.pack()
+
+#label idade e entry
+label_idade = tk.Label(janela,text="Idade")
+label_idade.pack()
+entry_idade = tk.Entry(janela)
+entry_idade.pack()
+
+btn_novo_aluno = tk.Button(janela, text="Adicionar",
+                            command=adicionar_usuario)
+btn_novo_aluno.pack()
+
+janela.mainloop()
